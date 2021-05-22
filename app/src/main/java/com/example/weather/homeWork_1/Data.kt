a3ab43a147ff0b2f158b5395f585a65b5db4f491
Aleks <kuriatae@mail.ru>
2021-05-22 23:12:05 +0700

    реализован DataClass, Object

--- app/src/main/java/com/example/weather/homeWork_1/Data.kt
+++ app/src/main/java/com/example/weather/homeWork_1/Data.kt
@@ -1,8 +1,5 @@
 package com.example.weather.homeWork_1
 
-data class Data(val city: String, val temperature: Double) {
-    val CITY: String = "city"
-    val TEMPERATURE: String = "temperature"
+data class Data(val city: String, val temperature: Double)
 
 
-}
